c1e55925b25aca890441dbb547cf30c5cc3fd76a
Samuel Victor Hill Santos <samuel.00000860090@unicap.br>
2026-03-10 23:10:17 +0000

    Criação do titulo, ajuste com className, e criação da bio

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,15 @@ import Profile from "@/components/Profile"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <header className="mb-10">
+        <h6 className="text-sm uppercase tracking-widest">
+          Site criado para a disciplina Programação Front-End
+        </h6>
+      </header>
+
       <Profile/>
+
     </div>
   );
 }
--- components/Profile.tsx
+++ components/Profile.tsx
@@ -2,22 +2,30 @@ import Image from 'next/image'
 
 export default function Profile(){
   return(
-    <div id='ProfileContainer' >
-      <div id='Profile'>
-        <img src="/Profile_Pic.png" 
+    <div id='ProfileContainer'>
+
+      <div id='Profile' className='flex flex-col items-center'>
+        <Image src="/Profile_Pic.png" 
         alt="Profile Pic" 
         width={150} 
         height={150}
         // Ajuste de Aspect Ratio e Crop para evitar distorção
-        className='w-30 h-30 rounded-full object-cover border-2 border-zinc-200'
+        className='w-[150px] h-[150px] rounded-full object-cover border-2 border-zinc-200'
         />
-        <h2 id='Nome' className='text-x1 font-bold mt-2'>
+      
+        <h2 id='Nome' className='text-xl font-bold mt-4'>
           Samuel Victor
         </h2>
       </div>
 
-      <div id='Bio'>
+      <div id='Bio' className='mt-2 text-center'>
+        <p className="text-zinc-600 dark:text-zinc-400">
+          Estudante da UNICAP, com foco em otimização e eficiência de sistemas 
+          junto a logica e algoritimos.
+          Construindo o caminho para a Engenharia de Software e DevOps.
+        </p>
       </div>
+
     </div>
   );
 }
